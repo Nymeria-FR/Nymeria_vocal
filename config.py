2e529d2e99d330893bc3d2fb066175241078dc33
Valeran-M <v.maytie85@orange.fr>
2021-07-11 16:32:07 +0200

    add base config

---
+++ config.py
@@ -0,0 +1,27 @@
+import os, toml
+
+class Config:
+    def get_path(self, name):
+        return os.path.join(os.path.dirname(os.path.realpath(__file__)), name)
+
+    def extract_config(self, file_name, template_name):
+        config_file = self.get_path(file_name)
+        if not os.path.isfile(config_file):
+            print(f"config {file_name} doesn't exist, copying template!")
+            shutil.copyfile(self.get_path(template_name), config_file)
+        return config_file
+
+class TomlConfig(Config):
+    def __init__(self, file_name, template_name):
+        config_file = self.extract_config(file_name, template_name)
+        self.load_config(config_file)
+
+    def load_config(self, config_file):
+        config = toml.load(config_file)
+        bot = config["bot"]
+        self.bot = bot["bot"]
+        self.token = bot["token"]
+        self.csv_data_base = bot["csv_data_base"]
+
+        self.servers = condif["servers"]
+        
